666d34e7d846ec13e22e874d7130a6300808a5e4
Brendon_Brink <brendon.brink@gmail.com>
2020-01-24 06:05:24 +0100

    Updates

---
+++ precache-manifest.3d865ccb4ba2aa631be40e77561fb04f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94a5e9ad5749c1d5d278d7a13c29334",
+    "url": "/WB_ADMIN2/index.html"
+  },
+  {
+    "revision": "da168e295ff0757c8489",
+    "url": "/WB_ADMIN2/static/css/2.88a9e19c.chunk.css"
+  },
+  {
+    "revision": "eafabd3d36623574bd15",
+    "url": "/WB_ADMIN2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "da168e295ff0757c8489",
+    "url": "/WB_ADMIN2/static/js/2.91c42fac.chunk.js"
+  },
+  {
+    "revision": "0bc7b1a32b50055ba5011d82f01aa9a5",
+    "url": "/WB_ADMIN2/static/js/2.91c42fac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eafabd3d36623574bd15",
+    "url": "/WB_ADMIN2/static/js/main.d608ff10.chunk.js"
+  },
+  {
+    "revision": "8a575d8a9d3eded0c5c4",
+    "url": "/WB_ADMIN2/static/js/runtime-main.29f81f70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WB_ADMIN2/static/media/logo.5d5d9eef.svg"
+  }
+]);
